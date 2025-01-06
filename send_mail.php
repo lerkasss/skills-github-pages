0a8bda8fb30dd8bc76c8b7cb45e9892bd6bc2711
lerkasss <soldatchenkova.valeriya@gmail.com>
2025-01-06 14:26:01 -0800

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $question = htmlspecialchars($_POST['question']);
 
     // Адрес почты, на который будет отправлено письмо
-    $to = "your-email@example.com";
+    $to = "contact@valeriiatranslator.com";
     
     // Тема письма
     $subject = "Новый контактный запрос от $name";
